19d6112fd88d014a67627373c8a5fb117e041aeb
DBomber60 <davidbreynolds@gmail.com>
2021-02-01 20:31:55 -0500

    ui updates

--- shinyex2.R
+++ shinyex2.R
@@ -25,35 +25,56 @@ ui <- fluidPage(
       withMathJax(helpText("$$G$$ 
                            $$\\sigma \\sim $$"))
     ),
-    mainPanel("here is some text explaining",
-              withMathJax(helpText("$$u \\sim N(0, \\sigma^2_u) $$
+    mainPanel(withMathJax(helpText("$$u \\sim N(0, \\sigma^2_u) $$
                                    $$X_1 \\sim N(0, \\sigma^2_x)$$ 
                                    $$g(P(Z_1 \\vert X_1)) = -0.1 + a x_1 $$
                                    $$P(X_2 \\vert X_1, Z_1) \\sim N(x_1 + b Z_1 + u, \\sigma^2_x/16) $$
                                    $$g(P(Z_2 \\vert Z_1, X_2)) = -0.1 + 2 z_1 + c x_2 $$
                                    $$g(P(Y \\vert Z_1, Z_2, Z_3, X_2)) = -0.1 + \\theta \\sum_{k=1}^3 z_k + d x_2 + u $$")),
-              plotOutput("hist"),
+    plotOutput("hist"),
     plotOutput("g"),
-    textOutput("text"))
+    h4("Special Cases"),
+    p("- d=0; The effect, theta, is equal to -0.25. In this case, the effect estimates from traditional models (logistic regression in this case) and causal models can be compared.
+      However, when d != 0, some of the effect of treatment is mediated through X2. In this case, the causal effect includes the effect transmitted through X2. Therefore, casual effect estimates can't be directly compared with 
+      logistic regression parameters since the latter are conditioned on X2."),
+    p("- U=0; no unmeasured confounders. When d=0, logistic regression and causal methods provide unbiased estimates of the true effect (-0.25). When d != 0, logistic regression model provides a conditional causal effect and causal models provide a marginal causal effect."),
+    p("- U != 0; this is when causal models should be used! Conditioning on X2 induces a non-causal association between Z1 and Y (through U).")
+    
+      
+    
+    )
   )
 )
 
 # Define server logic ----
 server <- function(input, output) {
   output$g = renderPlot({
-    n=7
-    gt = graph(n, edges = c(1,2))
-    V(gt)$name = c("X0", "A1", "X1", "A2", "X3", "A3", "Y")
-    plot(gt)
+    n = 6
+    
+    e = c(1,2, # X0 - A1
+          1,3, # X0 
+          2,3,
+          2,5,
+          3,4, # tvc
+          3,5,
+          4,5,
+          6,3,
+          6,5)
+    
+    gt = make_graph(n, edges = e)
+    
+    V(gt)$name = c("X1", "Z1", "X2", "Z2", "Y","U")
     V(gt)$size <- 30
+    V(gt)$frame.color = c(rep("black", 5), "white")
     E(gt)$weight = 1
     
     
-    vertex.color= c( rep(c("dodgerblue","red4"),3), "yellow")
-    
+    vertex.color= c( rep(c("dodgerblue","red4"),2), "yellow", "grey")
+    set.seed(12)
     plot(gt, vertex.label.color= c("black","white"), 
          vertex.label.dist=0,
          vertex.color = vertex.color,
+         edge.label = c("a", "", "b", "", "c", "d", "", "" ),
          vertex.label.cex= 1.5, vertex.shape = c("circle", "square"))
     
   })
@@ -71,10 +92,6 @@ server <- function(input, output) {
       scale_color_manual(values=c('#999999','#E69F00'))
     # hist(rnorm(10))
     } )
-  
-  output$text <- renderText({ 
-    "General Setup"
-  })
 
 }
 
